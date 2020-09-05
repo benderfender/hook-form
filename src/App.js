94c151772eaafeb76f30ef5114c054a66a319206
benderfender <67402670+benderfender@users.noreply.github.com>
2020-09-05 17:45:16 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import React,{useState} from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import UserForm from './component/UserForm';
-//import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [state, setState] = useState({
-    firstname: "",
-    lastname: "",
-    email: "",
-    password: "",
-    cwpass: ""
-  });
-
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
-      <UserForm inputs{...state} firstname="" lastname="" email="" password="" cwpass=""/>
+      <div>
+      <UserForm />
+      </div>
     </div>
   );
 }
--- src/component/UserForm.jsx
+++ src/component/UserForm.jsx
@@ -1,52 +1,99 @@
 import React, {useState} from 'react';
 
 const UserForm = (props) => {
-    const [firstname, setFirstname] = useState("");
-    const [lastname, setLastname] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [cwpass, setConfirmPassword] = useState("");
+    
+    const [regform,setRegform] = useState({
+        firstname: "",
+        lastname: "",
+        email: "",
+        password: "",
+        cwpass: ""
+    });
+
+    const[display,setDisplay] = useState(null);
+
+    const handleRegform = (e) => {
+        setRegform({
+            ...regform,
+            [e.target.name]: e.target.value
+        })
+    }
 
     const createUser = (e) => {
         e.preventDefault();
-        const newUser = {firstname, lastname, email, password, cwpass};
-        console.log("Welcome", newUser);
+        setDisplay(regform);
+        setRegform({
+            firstname: "",
+            lastname: "",
+            email: "",
+            password: "",
+            cwpass: ""
+        });
     };
+
+    const nameValid = (name) => {
+        if(name.length <2 && name.length !==0) {
+            return false;
+        } 
+        return true;
+    }
+    const passwordValid = (name) => {
+        if(name.length <4 && name.length !==0) {
+            return false;
+        } 
+        return true;
+    }
+    const cwpassValid = (name) => {
+        if(regform.password !== regform.cwpass && name.length !==0) {
+            return false;
+        } 
+        return true;
+    }
 
     return(
         <div>
-            <form onSubmit = {createUser} className="">
+            <form onSubmit = {createUser} className="col-5 mx-auto my-5">
+                <h2>Registeration Form</h2>
                 <div className="form-group">
                     <label>First Name </label>
-                    <input className="form-control" type="text" value={firstname} onChange={(e)=>setFirstname(e.target.value)}/>
+                    <input className="form-control" type="text" name="firstname" value={regform.firstname} onChange={handleRegform} placeholder="Enter Your First Name"/>
+                    {nameValid(regform.firstname)?"":<p class="alert alert-danger" role="alert">Must contain at least 2 characters!!!!</p>}
                 </div>
                 <div className="form-group">
                     <label>Last Name </label>
-                    <input className="form-control" type="text" value={lastname} onChange={(e)=>setLastname(e.target.value)}/>
+                    <input className="form-control" type="text" name="lastname" value={regform.lastname} onChange={handleRegform} placeholder="Enter Your Last Name"/>
+                    {nameValid(regform.lastname)?"":<p class="alert alert-danger" role="alert">Must contain atleast 2 characters!!!!</p>}
                 </div>
                 <div className="form-group">
                     <label>Email </label>
-                    <input className="form-control" type="text" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+                    <input className="form-control" type="text" name="email" value={regform.email} onChange={handleRegform} placeholder="Enter Your Email"/>
+                    {nameValid(regform.email)?"":<p class="alert alert-danger" role="alert">Must contain atleast 2 characters!!!!</p>}
                 </div>
                 <div className="form-group">
                     <label>Password </label>
-                    <input className="form-control" type="text" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+                    <input className="form-control" type="text" name="password" value={regform.password} onChange={handleRegform} placeholder="Enter Your Password"/>
+                    {passwordValid(regform.password)?"":<p class="alert alert-danger" role="alert">Must be atleast 4 characters long!!!!</p>}
                 </div>
                 <div className="form-group">
                     <label>Confirm Password </label>
-                    <input className="form-control" type="text" value={cwpass} onChange={(e)=>setConfirmPassword(e.target.value)}/>
+                    <input className="form-control" type="text" name="cwpass" value={regform.cwpass} onChange={handleRegform} placeholder="Confirm Password"/>
+                    {cwpassValid(regform.cwpass)?"":<p class="alert alert-danger" role="alert">Not Matching!!!!</p>}
                 </div>
+                <input type="submit" class="btn btn-dark btn-outline-light btn-block" value="Register" />
             </form>
-            <div>
-                <h4>Your Form Data</h4>
-                <p>First Name: {firstname}</p>
-                <p>Last Name: {lastname}</p>
-                <p>Email: {email}</p>
-                <p>Password: {password}</p>
-            </div>
-
+            {
+                display ?
+                <div class="alert alert-success" role="alert">
+                    <h4 class="alert-heading">Your Form Data</h4>
+                    <p>First Name: {display.firstname}</p>
+                    <p>Last Name: {display.lastname}</p>
+                    <p>Email: {display.email}</p>
+                    <p>Password: {display.password}</p>
+                </div> : null
+            }
         </div>
     )
 }
+
 
 export default UserForm;
